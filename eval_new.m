fdc64b9fe6179dc4ddbc6c728912a6151ee4a75c
federico <f.rossi57@studenti.unipi.it>
2022-11-06 11:24:27 +0100

    Added lz2z mapping. [beta]

--- eval_new.m
+++ eval_new.m
@@ -15,6 +15,7 @@ resx = int64(res(1:Nx));
 resy = int64(res(Nx+1:2*Nx));
 solution = struct;
 
+
 solution.op = getFunctionName(op);
 solution.ophandle = op;
 solution.p  = plist;
@@ -22,6 +23,9 @@ solution.optab = ptab;
 solution.Lx = resx;
 solution.Ly = resy;
 solution.Lz = resx + resy';
+
+lz2z = genLz2z(solution.Lz,solution.optab,solution.p)
+
 
 solution
 
@@ -34,6 +38,34 @@ function name = getFunctionName(op)
     end
 end
 
-function lz2z = genLz2z(Lz,optab)
-    lz2z = [];
+function lz2z = genLz2z(Lz,optab,plist)
+    % We map the Lz set to the original z (optab) set
+    % We want to discard duplicates, since they map to the
+    % same element in z
+    % Elements of optab may not be present in z (round to nearest)
+
+    r = size(optab,1);
+    c = size(optab,2);
+    
+    lz2zKeys = [];
+    lz2zVals = [];
+    lz2z = struct;
+    for i=1:r
+        for j=1:c
+            p = optab(i,j);
+
+            % closest posit and relative index
+            [~, idx] = min(abs(plist-p));
+            minp = plist(idx);
+
+            % map idx to correspondent z (only if not contained)
+            lzv = Lz(i,j);
+            if ~ismember(lzv,lz2zKeys)
+                lz2zKeys = [lz2zKeys; lzv];
+                lz2zVals = [lz2zVals; minp];
+            end
+        end
+    end
+    lz2z.keys = lz2zKeys;
+    lz2z.vals = lz2zVals;
 end
